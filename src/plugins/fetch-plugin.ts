d4e8f3884d6fd2b3297f91491fa9645cadcfe1d0
Rachit Parwanda <parwandarachit1601@gmail.com>
2022-01-12 17:32:55 +0530

    Initial ESBuild setup

---
+++ src/plugins/fetch-plugin.ts
@@ -0,0 +1,67 @@
+import * as esbuild from 'esbuild-wasm'
+import axios from 'axios'
+import localForage from 'localforage'
+
+const fileCache = localForage.createInstance({
+	name: 'filecache',
+})
+
+export const fetchPlugin = (inputCode: string) => {
+	return {
+		name: 'fetch-plugin',
+		setup(build: esbuild.PluginBuild) {
+			build.onLoad({ filter: /^index\.js$/ }, () => {
+				return {
+					loader: 'jsx',
+					contents: inputCode,
+				}
+			})
+
+			// ESBuild will fall-through this, if nothing returned
+			build.onLoad({ filter: /.*/ }, async (args: any) => {
+				// check for cache
+				const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path)
+				if (cachedResult) {
+					return cachedResult
+				}
+			})
+
+			build.onLoad({ filter: /.css$/ }, async (args: any) => {
+				const { data, request } = await axios.get(args.path)
+				const escapedData = data.replace(/\n/g, '').replace(/"/g, '\\"').replace(/'/g, "\\'")
+
+				const contents = `
+          const style = document.createElement('style');
+          style.innerText = '${escapedData}';
+          document.head.appendChild(style);
+        `
+
+				const result: esbuild.OnLoadResult = {
+					loader: 'jsx',
+					contents,
+					resolveDir: new URL('./', request.responseURL).pathname,
+				}
+
+				// save to cache
+				await fileCache.setItem(args.path, result)
+
+				return result
+			})
+
+			build.onLoad({ filter: /.*/ }, async (args: any) => {
+				const { data, request } = await axios.get(args.path)
+
+				const result: esbuild.OnLoadResult = {
+					loader: 'jsx',
+					contents: data,
+					resolveDir: new URL('./', request.responseURL).pathname,
+				}
+
+				// save to cache
+				await fileCache.setItem(args.path, result)
+
+				return result
+			})
+		},
+	}
+}
